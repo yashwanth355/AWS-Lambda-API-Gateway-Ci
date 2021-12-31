e9028c38f67f53db039d2d0a98bd01cda2eb4e8b
Yashwanth <ykotha@dginosolutions.com>
2021-12-31 12:15:26 +0530

    Update the changes

--- Project/Source/hello/main.go
+++ Project/Source/hello/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-lambda-go/lambda"
-	"https://github.com/yashwanth355/AWS-Lambda-API-Gateway-Ci.git/source/hello/handler"
+	"github.com/yashwanth355/AWS-Lambda-API-Gateway-Ci/Source/hello/handler"
 )
 
 func main() {
