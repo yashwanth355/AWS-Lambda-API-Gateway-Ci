b02e1223433d0b3ff61f9cb2581c9273dc741ce4
yashwanth355 <95912283+yashwanth355@users.noreply.github.com>
2022-01-01 17:43:57 +0530

    Update lambda.tf

--- Project/Infrastructure/lambda.tf
+++ Project/Infrastructure/lambda.tf
@@ -1,20 +1,18 @@
-resource "aws_lambda_function" "example" {
-   function_name        = "ServerlessExample"
-# The bucket name as created earlier with "aws s3api create-bucket"
-   s3_bucket            = "terraform-serverless-example"
-   s3_key               = "v1.0.0/example.zip"
-# "main" is the filename within the zip file (main.js) and "handler"
-   # is the name of the property under which the handler function was
-   # exported in that file.
-   handler              = "main.handler"
-   runtime              = "Go 1.x"
-role                    = aws_iam_role.lambda_exec.arn
+resource "aws_lambda_function" "time" {
+  function_name    = "time"
+  filename         = "aws-lambda-go.zip"
+  handler          = "main"
+  source_code_hash = "data.archive_file.zip.output_base64sha256"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  runtime          = "go1.x"
+  memory_size      = 128
+  timeout          = 10
 }
 # IAM role which dictates what other AWS services the Lambda function
  # may access.
  
-resource "aws_iam_role" "lambda_exec" {
-   name                  = "serverless_example_lambda"
+resource "aws_iam_role" "iam_for_lambda" {
+   name                  = "iam_for_lambda"
 assume_role_policy       = <<EOF
 {
   "Version": "2012-10-17",
@@ -32,12 +30,49 @@ assume_role_policy       = <<EOF
 EOF
 }
 
-resource "aws_lambda_permission" "apigw" {
-   statement_id            = "AllowAPIGatewayInvoke"
-   action                  = "lambda:InvokeFunction"
-   function_name           = aws_lambda_function.example.function_name
-   principal               = "apigateway.amazonaws.com"
-# The "/*/*" portion grants access from any method on any resource
-   # within the API Gateway REST API.
-   source_arn              = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+resource "aws_api_gateway_rest_api" "api" {
+  name = "time_api"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "time"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.time.invoke_arn}"
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.time.function_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+}
+
+resource "aws_api_gateway_deployment" "time_deploy" {
+  depends_on = [aws_api_gateway_integration.integration]
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "v1"
+
+}
+
+output "url" {
+  value = "${aws_api_gateway_deployment.time_deploy.invoke_url}${aws_api_gateway_resource.resource.path}"
 }
