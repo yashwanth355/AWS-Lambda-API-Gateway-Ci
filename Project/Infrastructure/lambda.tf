d474f25d5071d1a1a8d0d0396606627b16e92399
Yashwanth <ykotha@dginosolutions.com>
2022-01-06 10:41:38 +0530

    updated

--- Project/Infrastructure/lambda.tf
+++ Project/Infrastructure/lambda.tf
@@ -37,30 +37,8 @@ assume_role_policy       = <<EOF
 EOF
 }
 
-resource "aws_api_gateway_rest_api" "api" {
+data "aws_api_gateway_rest_api" "time_api" {
   name = "time_api"
-}
-
-resource "aws_api_gateway_resource" "resource" {
-  path_part   = "time"
-  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-}
-
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
-  resource_id             = "${aws_api_gateway_resource.resource.id}"
-  http_method             = "${aws_api_gateway_method.method.http_method}"
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.time.invoke_arn}"
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
