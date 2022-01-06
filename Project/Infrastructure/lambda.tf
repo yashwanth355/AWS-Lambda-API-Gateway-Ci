e1d9386ef39380268bb6ed9350164ea0ca182b8f
Yashwanth <ykotha@dginosolutions.com>
2022-01-06 11:22:55 +0530

    updated

--- Project/Infrastructure/lambda.tf
+++ Project/Infrastructure/lambda.tf
@@ -37,8 +37,30 @@ assume_role_policy       = <<EOF
 EOF
 }
 
-data "aws_api_gateway_rest_api" "time_api" {
+resource "aws_api_gateway_rest_api" "api" {
   name = "time_api"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "time"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.time.invoke_arn}"
 }
 
 resource "aws_lambda_permission" "apigw_lambda" {
@@ -53,7 +75,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
 resource "aws_api_gateway_deployment" "time_deploy" {
   depends_on = [aws_api_gateway_integration.integration]
 
-  rest_api_id = "time_api"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "test"
 
 }
